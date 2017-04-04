dc7b2181cebc7974acf2575afa9b50edbc33c888
Wanderson <bsilva.wanderson@gmail.com>
2017-04-03 23:14:40 -0300

    estabelecando uma comunicação básica entre criente e servidor

--- cliente_banco_cooperativo/src/comunicacao/TesteCliente.java
+++ cliente_banco_cooperativo/src/comunicacao/TesteCliente.java
@@ -1,7 +1,30 @@
 package comunicacao;
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.*;
+
 /**
  * Created by wanderson on 03/04/17.
  */
 public class TesteCliente {
+
+    public static void main(String args[]) throws Exception {
+        String mensagem;
+        String mensagemModificada;
+
+        BufferedReader entradaCliente = new BufferedReader(new InputStreamReader(System.in));
+        Socket cliente = new Socket("192.168.15.10",7777);
+        DataOutputStream outToServer = new DataOutputStream(cliente.getOutputStream());
+        BufferedReader entradaServidor = new BufferedReader(
+                new InputStreamReader(cliente.getInputStream()));
+        mensagem = entradaCliente.readLine();
+        outToServer.writeBytes(mensagem + "\n");
+        mensagemModificada = entradaServidor.readLine();
+
+        System.out.println("Mesagem do servidor: "+mensagemModificada);
+        cliente.close();
+
+    }
 }
--- servidor_banco_cooperativo/src/comunicacao/TesteServidor.java
+++ servidor_banco_cooperativo/src/comunicacao/TesteServidor.java
@@ -1,9 +1,35 @@
 package comunicacao;
 
-public class Comunicacao {
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
 
-    public static void main (String args[]){
+public class TesteServidor {
 
+    public static void main (String args[]) throws Exception {
+
+        String mensagemCliente;
+        String mensagemModificada;
+
+        System.out.println("Iniciando servidor...");
+        ServerSocket servidor = new ServerSocket(7777);
+        System.out.println("Servidor iniciado");
+
+        while (true){
+            Socket connectionSocker = servidor.accept();
+
+            BufferedReader entradaCliente = new BufferedReader(
+                    new InputStreamReader(connectionSocker.getInputStream()));
+
+            DataOutputStream outToCliente = new DataOutputStream
+                    (connectionSocker.getOutputStream());
+
+            mensagemCliente = entradaCliente.readLine();
+            mensagemModificada = mensagemCliente.toUpperCase() + "\n";
+            outToCliente.writeBytes(mensagemModificada+"\n");
+        }
     }
 
 }
